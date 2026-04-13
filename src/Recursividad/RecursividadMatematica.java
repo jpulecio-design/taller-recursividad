103b15bbb0d8e1355698e82f5681521ecccbbb9a
jpulecio-diseño <jpulecio@unimayor.edu.co>
2026-04-12 22:13:58 -0500

    feat: Optimice comentarios y parte del codigo de la Clase RecursividadMatematica

--- src/Recursividad/RecursividadMatematica.java
+++ src/Recursividad/RecursividadMatematica.java
@@ -1,37 +1,34 @@
 package Recursividad;
 
 public class RecursividadMatematica {
-    // Ej 7 - MCD Euclides O(log(n))
-    // Caso base: n == 0 → m
+    // Ej 7 - MCD Euclides
+    // Complejidad: O(log n)
+    // Caso base: n == 0 -> retorna m
     public static int calcularMcd(int m, int n) {
-        boolean esCasoBase = (n == 0);
-        if (esCasoBase)
+        if (n == 0)
             return m;
         return calcularMcd(n, m % n);
     }
 
-    // Ej 13 - Fibonacci O(2^n)
-    // Caso base: n==0 → 0, n==1 → 1
+    // Ej 13 - Fibonacci
+    // Complejidad: O(2^n)
+    // Caso base: n==0 -> retorna 0 | n == 1 -> retorna 1
     public static int calcularFibonacci(int n) {
-        boolean esPrimerCasoBase = (n == 0);
-        boolean esSegundoCasoBase = (n == 1);
-        if (esPrimerCasoBase)
+        if (n == 0)
             return 0;
-        if (esSegundoCasoBase)
+        if (n == 1)
             return 1;
         return calcularFibonacci(n - 1) + calcularFibonacci(n - 2);
     }
 
-    // Ej 14 - Ackermann O(mayor que exponencial)
-    // Caso base 1: m == 0 → n + 1
-    // Caso base 2: m>0, n == 0 → ackermann(m-1, 1)
-    // Caso recursivo → ackermann(m-1, ackermann(m, n-1))
+    // Ej 14 - Ackermann
+    // Complejidad: O(c^n)
+    // Caso base 1: m == 0 -> retorna n + 1
+    // Caso base 2: m>0, n == 0 -> retorna calcularAckermann(m-1, 1)
     public static int calcularAckermann(int m, int n) {
-        boolean esPrimerCasoBase = (m == 0);
-        boolean esSegundoCasoBase = (m > 0 && n == 0);
-        if (esPrimerCasoBase)
+        if (m == 0)
             return n + 1;
-        if (esSegundoCasoBase)
+        if (n == 0)
             return calcularAckermann(m - 1, 1);
         return calcularAckermann(m - 1, calcularAckermann(m, n - 1));
     }
